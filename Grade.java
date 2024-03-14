eeecbe9698adc2db44c12a2b9698d7d3fe4d6b02
Shivsharan16 <139864637+Shivsharan16@users.noreply.github.com>
2024-03-14 15:03:20 +0530

    oopj

---
+++ Grade.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Grade
+{
+   public static void main(String[] args)
+   {
+      int  score;
+ char grade;
+Scanner console = new Scanner(System.in);
+System.out.print("Enter your numeric test score : ");
+      score = console.nextInt(); 
+if (score >= 90)
+      {
+         grade = 'A';
+      }
+      else if (score >= 80)
+      {
+         grade = 'B';
+      }
+      else if (score >= 70)
+      {
+         grade = 'C';
+      }
+      else if (score >= 50)
+      {
+         grade = 'D';
+      }
+System.out.println("Your grade is " + grade);
+   }
+}
